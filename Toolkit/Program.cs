78c9ff9b9360b12ea92d98f3f257805948ddfec7
Daniel Castel De Oro <83740100+DanielCastelDeOro@users.noreply.github.com>
2021-06-22 13:57:06 -0600

    commit 2

--- Toolkit/Program.cs
+++ Toolkit/Program.cs
@@ -4,17 +4,48 @@ namespace Toolkit
 {
     class Entry
     {
+        public string url;
+            public string name;
+        public string key;
 
+        public Entry (string url, string name, string key)
+        {
+            url = newUrl;
+            name = newName;
+            key = newKey;
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
+            Entry[] entryies = new Entry[]
+            {
+                new Entry("www.facebook.com", "Facebook", "f"),
+                new Entry("www.google.com", "Google", "g"),
+                new Entry("www.youtube.com", "YouTube", "y")
+            };
+
             Console.WriteLine("Wright the key and press enter");
             Console.WriteLine("Options");
-            Console.WriteLine("\t f - Facebook!");
-            Console.WriteLine("\t g - Google!");
-            Console.WriteLine("\t y YouTube!");
+            foreach (Entry entry in entryies)
+            {
+                Console.WriteLine("\t" + entry.key + "-" + entry.name);
+            }
+
+            Console.WriteLine("\nYour Option?");
+
+            var userInput = Console.ReadLine();
+
+            foreach (Entry entry1 in entryies)
+            {
+                if (userInput == Entry.key)
+                {
+
+                }
+            }
+
+            Console.ReadLine();
         }
     }
 }
