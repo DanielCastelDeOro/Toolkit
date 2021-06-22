45b4219b51ae5e4cdfbd2f88a071a217be12c731
Daniel Castel De Oro <83740100+DanielCastelDeOro@users.noreply.github.com>
2021-06-22 05:10:45 -0600

    Add project files.

---
+++ Toolkit/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Toolkit
+{
+    class Entry
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wright the key and press enter");
+            Console.WriteLine("Options");
+            Console.WriteLine("\t f - Facebook!");
+            Console.WriteLine("\t g - Google!");
+            Console.WriteLine("\t y YouTube!");
+        }
+    }
+}
